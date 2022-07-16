9349921d6a01baa2d874fc234d6755bd293180aa
walter <walter.1116@hotmail.com>
2022-07-16 11:21:54 -0300

    componentes do layout finalizacao

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
-import BotaoDeNotificacao from './componentes/BotaoDeNotificacao'
+import Header from './componentes/Header'
+import SalesCard from './componentes/SalesCard/SalesCard'
 
 function App() {
   return(
+
     <>
-    <h1>Ola mundo!</h1>
-    <BotaoDeNotificacao />
+    <Header />
+      <main>
+        <section id="sales">
+          <div className="dsmeta-container">
+            <SalesCard />
+         </div>
+        </section>
+    </main>
     </>
+      
   )
 }
 
